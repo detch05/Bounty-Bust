2bb3303f76c7f6e07ecd27f1f851fdf4f4807ad9
Archer <josemiguelarcher@gmail.com>
2025-11-24 13:40:30 +0000

    Aside is now a partial as it will be used a lot

--- Project/resources/views/pages/home.blade.php
+++ Project/resources/views/pages/home.blade.php
@@ -2,42 +2,6 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row ">
-            <aside class="col col-md-2 border-end min-vh-100 ms-3 p-3" id="contentNav">
-                <nav class="nav flex-column">
-                    <section id="mainFeatures">
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-house-door-fill fs-4"></i>
-                            <a class="nav-link" href="/">Home</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-bullseye fs-4"></i>
-                            <a class="nav-link" href="#">Bounties</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-tags-fill fs-4"></i>
-                            <a class="nav-link" href="#">Tags</a>
-                        </div>
-                    </section>
-                    <section id="misc">
-                        <div class="d-flex align-items-center">
-                           <i class="bi bi-question-circle-fill fs-4"></i>
-                            <a class="nav-link" aria-current="page" href="#">Tour</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-briefcase-fill fs-4"></i>
-                            <a class="nav-link" href="#">About Us</a>
-                        </div>
-                        <div class="d-flex align-items-center">
-                            <i class="bi bi-envelope-fill fs-4"></i>
-                            <a class="nav-link" href="#">Contacts</a>
-                        </div>
-                    </section>
-                </nav>
-            </aside>
-
-        </div>
-
+          @include('partials.aside')
     </div>
-
 @endsection
