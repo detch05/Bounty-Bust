7ab3ebf576c2304270250efaa1208590a25c3827
Archer <josemiguelarcher@gmail.com>
2025-11-28 20:16:21 +0000

    Small change in docker image config

--- Project/upload_image.sh
+++ Project/upload_image.sh
@@ -22,7 +22,7 @@
 set -euo pipefail
 
 # Replace with your group's image name
-IMAGE_NAME=gitlab.up.pt:5050/lbaw/lbaw2025/lbaw25146
+IMAGE_NAME=gitlab.up.pt:5050/lbaw/lbaw2526/lbaw25146
 
 # Ensure that dependencies are available
 composer install
